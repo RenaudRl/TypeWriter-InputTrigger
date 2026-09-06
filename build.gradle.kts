5f53fda7a2f76c07c69e2d4a134d4ca5616f161d
renaud <rlpirou@gmail.com>
2026-09-06 18:29:44 +0200

    chore: migrate extension to Typewriter 0.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.3.20"
-    id("com.typewritermc.module-plugin") version "2.1.0"
+    kotlin("jvm") version "2.2.10"
+    id("com.typewritermc.module-plugin") version "2.2.0"
 }
 
 repositories {
@@ -10,7 +10,11 @@ repositories {
 }
 
 group = "btc.renaud"
-version = "0.0.3"
+version = "0.0.4"
+
+base {
+    archivesName.set("InputTriggerExtension")
+}
 
 typewriter {
     namespace = "btcrenaud"
@@ -18,7 +22,7 @@ typewriter {
         name = "InputTrigger"
         shortDescription = "Track and trigger actions via player inputs, hotbar slots and inventory buttons."
         description = "A comprehensive TypeWriter extension for intercepting Minecraft key inputs (F, Q, Shift, Ctrl, T, /) and creating clickable inventory buttons with click-type actions, criteria, cooldowns, and crafting grid safety."
-        engineVersion = "0.9.0-beta-175"
+        engineVersion = "0.9.0-beta-176"
         channel = com.typewritermc.moduleplugin.ReleaseChannel.BETA
         paper()
     }
